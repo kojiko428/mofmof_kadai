581480298c98f7bba1362cad316796e6e13de36f
koji_yamamoto <koji_ko428@yahoo.co.jp>
2021-03-08 07:03:49 +0000

    課題提出(5)

--- db/migrate/20210307160833_create_stations.rb
+++ db/migrate/20210307160833_create_stations.rb
@@ -1,12 +1,9 @@
 class CreateStations < ActiveRecord::Migration[5.2]
   def change
     create_table :stations do |t|
-      t.string :name_of_railway_line1
-      t.string :statation_name1
-      t.integer :how_many_minutes_walks1
-      t.string :name_of_railway_line2
-      t.string :statation_name2
-      t.integer :how_many_minutes_walks2
+      t.string :name_of_railway_line
+      t.string :statation_name
+      t.integer :how_many_minutes_walks
 
       t.timestamps
     end
